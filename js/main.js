bda9343810a5638533ee018f7e2915f06b10a71d
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-09-26 14:09:41 +0200

    Create site

--- js/main.js
+++ js/main.js
@@ -1,78 +1,117 @@
-var siteLoader = function () {
-    var r = '';
-    // enum
-    var status = Status.Online;
-    // literal type
-    var mainColor;
-    mainColor = "green";
-    var score;
-    score = 55;
-    score = 'five';
-    if (typeof score === "string") {
-        score = "\"" + score + "\"";
-    }
-    var rand = qmat_getRandomNumber(1, 2);
-    if (rand === 1) {
-        status = Status.Offline;
-    }
-    else {
-        status = Status.Online;
-    }
-    if (status === Status.Offline) {
-        r += qstr_capitalizeFirstLetter('Status is offline.');
-    }
-    else {
-        r += qstr_capitalizeFirstLetter('Status is online.');
-    }
-    r += '<hr/>';
-    r += "<ul>";
-    var dataReader = new DataReader();
-    dataReader.getQuotes().forEach(function (m) {
-        r += "<li style=\"color: " + mainColor + "\">" + m + "</li>";
-    });
-    r += "</ul>";
-    r += '<hr/>';
-    r += dataReader.showFlaschard({ front: 'house', back: 'Haus' });
-    r += dataReader.showFlaschard({ front: 'mouse', back: 'Maus' });
-    r += '<hr/>';
-    r += "The score is " + score + ".";
-    //return r;
-    var site = new Site();
-    return site;
-};
-// =========================================================
-var qstr_capitalizeFirstLetter = function (line) {
-    return line.charAt(0).toUpperCase() + line.slice(1);
-};
-var qmat_getRandomNumber = function (start, end) {
-    return Math.floor(Math.random() * end) + start;
-};
-// enum
-var Status;
-(function (Status) {
-    Status[Status["Online"] = 0] = "Online";
-    Status[Status["Offline"] = 1] = "Offline";
-})(Status || (Status = {}));
-// ES6 classes
 var Site = /** @class */ (function () {
     function Site() {
     }
     Object.defineProperty(Site.prototype, "title", {
         get: function () {
-            return 'the title';
+            return 'TypeScript/SASS Site';
         },
         enumerable: false,
         configurable: true
     });
     Object.defineProperty(Site.prototype, "content", {
         get: function () {
-            return 'content';
+            var r = '';
+            // === ENUMS ===================
+            var Status;
+            (function (Status) {
+                Status[Status["Online"] = 0] = "Online";
+                Status[Status["Offline"] = 1] = "Offline";
+            })(Status || (Status = {}));
+            // enum
+            var status = Status.Online;
+            var sdf = 34;
+            // literal type
+            var mainColor;
+            mainColor = 'green';
+            var score;
+            score = 55;
+            score = 'five';
+            if (typeof score === 'string') {
+                score = "\"" + score + "\"";
+            }
+            var rand = qmat.getRandomNumber(1, 2);
+            if (rand === 1) {
+                status = Status.Offline;
+            }
+            else {
+                status = Status.Online;
+            }
+            if (status === Status.Offline) {
+                r += qstr.capitalizeFirstLetter('Status is offline.');
+            }
+            else {
+                r += qstr.capitalizeFirstLetter('Status is online.');
+            }
+            r += '<hr/>';
+            r += "<ul>";
+            var dataReader = new DataReader();
+            dataReader.getQuotes().forEach(function (m) {
+                r += "<li style=\"color: " + mainColor + "\">" + m + "</li>";
+            });
+            r += "</ul>";
+            r += '<hr/>';
+            r += dataReader.showFlaschard({ front: 'house', back: 'Haus' });
+            r += dataReader.showFlaschard({ front: 'mouse', back: 'Maus' });
+            r += '<hr/>';
+            r += "The score is " + score + ".";
+            return r;
         },
         enumerable: false,
         configurable: true
     });
     return Site;
 }());
+// === SITE LOADER AREA =================================
+var siteLoader = function (option) {
+    if (option === void 0) { option = ''; }
+    if (option === '') {
+        var site = new Site();
+        return site;
+    }
+    else {
+        var site = new StartSite();
+        return site;
+    }
+};
+var StartSite = /** @class */ (function () {
+    function StartSite() {
+    }
+    Object.defineProperty(StartSite.prototype, "title", {
+        get: function () {
+            return 'Welcome';
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(StartSite.prototype, "content", {
+        get: function () {
+            return 'Welcome to this site.';
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return StartSite;
+}());
+// === QTOOLS ===================
+var qtools_qstr = /** @class */ (function () {
+    function qtools_qstr() {
+    }
+    qtools_qstr.prototype.capitalizeFirstLetter = function (line) {
+        return line.charAt(0).toUpperCase() + line.slice(1);
+    };
+    return qtools_qstr;
+}());
+var qtools_qmat = /** @class */ (function () {
+    function qtools_qmat() {
+        this.getRandomNumber = function (start, end) {
+            return Math.floor(Math.random() * end) + start;
+        };
+    }
+    return qtools_qmat;
+}());
+var qstr = new qtools_qstr();
+var qmat = new qtools_qmat();
+// === DATA SOURCE ===================
 var DataReader = /** @class */ (function () {
     function DataReader() {
     }
--- ts/main.ts
+++ ts/main.ts
@@ -1,92 +1,116 @@
-const siteLoader = () => { 
-	let r = '';
-
-	// enum
-	let status: Status = Status.Online;
-
-	// literal type
-	let mainColor: 'navy' | 'green' | 'purple';
-	mainColor = "green";
-
-	// union type
-	type FlexibleScore = number | string;
-	let score: FlexibleScore;
-	score = 55;
-	score = 'five';
-	if (typeof score === "string") {
-		score = `"${score}"`;
-	}
-
-	let rand = qmat_getRandomNumber(1,2);
-	if (rand === 1) {
-		status = Status.Offline
-	} else {
-		status = Status.Online;
-	}
-
-    if (status === Status.Offline) {
-        r += qstr_capitalizeFirstLetter('Status is offline.');
-    } else {
-        r += qstr_capitalizeFirstLetter('Status is online.');
-	}
-	
-	r += '<hr/>';
-
-	r += `<ul>`;
-	const dataReader = new DataReader();
-	dataReader.getQuotes().forEach(m => {
-		r += `<li style="color: ${mainColor}">${m}</li>`;
-	});
-	r += `</ul>`;
-
-	r += '<hr/>';
-
-	r += dataReader.showFlaschard({ front: 'house', back: 'Haus' });
-	r += dataReader.showFlaschard({ front: 'mouse', back: 'Maus' });
-
-	r += '<hr/>';
-
-	r += `The score is ${score}.`;
-
-	//return r;
-
-	const site = new Site();
-	return site;
-};
-
-// =========================================================
-
-const qstr_capitalizeFirstLetter = (line: string) => {
-    return line.charAt(0).toUpperCase() + line.slice(1);
-};
-const qmat_getRandomNumber = (start:number, end:number) => {
-	return Math.floor(Math.random() * end) + start;
-};
-
-// enum
-enum Status {
-    Online,
-    Offline
-}
-
-// interfaces
-interface IFlashcard {
-	front: string,
-	back: string
-	category?: string,
-	display?: () => string
-}
-
-// ES6 classes
-
 class Site {
-    get title() {
-        return 'the title';
-    }
-    get content() {
-        return 'content';
-    }
+	get title() {
+		return 'TypeScript/SASS Site';
+	}
+	get content() {
+		let r = '';
+
+		// === INTERFACES ===================
+
+		interface IFlashcard {
+			front: string;
+			back: string;
+			category?: string;
+			display?: () => string;
+		}
+
+		// === ENUMS ===================
+
+		enum Status {
+			Online,
+			Offline
+		}
+
+		// enum
+		let status: Status = Status.Online;
+		const sdf = 34;
+
+		// literal type
+		let mainColor: 'navy' | 'green' | 'purple';
+		mainColor = 'green';
+
+		// union type
+		type FlexibleScore = number | string;
+		let score: FlexibleScore;
+		score = 55;
+		score = 'five';
+		if (typeof score === 'string') {
+			score = `"${score}"`;
+		}
+
+		let rand = qmat.getRandomNumber(1, 2);
+		if (rand === 1) {
+			status = Status.Offline;
+		} else {
+			status = Status.Online;
+		}
+
+		if (status === Status.Offline) {
+			r += qstr.capitalizeFirstLetter('Status is offline.');
+		} else {
+			r += qstr.capitalizeFirstLetter('Status is online.');
+		}
+
+		r += '<hr/>';
+
+		r += `<ul>`;
+		const dataReader = new DataReader();
+		dataReader.getQuotes().forEach((m) => {
+			r += `<li style="color: ${mainColor}">${m}</li>`;
+		});
+		r += `</ul>`;
+
+		r += '<hr/>';
+
+		r += dataReader.showFlaschard({ front: 'house', back: 'Haus' });
+		r += dataReader.showFlaschard({ front: 'mouse', back: 'Maus' });
+
+		r += '<hr/>';
+
+		r += `The score is ${score}.`;
+
+		return r;
+	}
 }
+
+// === SITE LOADER AREA =================================
+
+const siteLoader = (option = '') => {
+	if (option === '') {
+		const site = new Site();
+		return site;
+	} else {
+		const site = new StartSite();
+		return site;
+	}
+};
+
+class StartSite {
+	get title() {
+		return 'Welcome';
+	}
+	get content() {
+		return 'Welcome to this site.';
+	}
+}
+
+// === QTOOLS ===================
+
+class qtools_qstr {
+	capitalizeFirstLetter(line: string) {
+		return line.charAt(0).toUpperCase() + line.slice(1);
+	}
+}
+
+class qtools_qmat {
+	getRandomNumber = (start: number, end: number) {
+		return Math.floor(Math.random() * end) + start;
+	}
+}
+const qstr = new qtools_qstr();
+const qmat = new qtools_qmat();
+
+// === DATA SOURCE ===================
 
 class DataReader {
 	getQuotes() {
@@ -94,7 +118,7 @@ class DataReader {
 			'this one',
 			'that one',
 			'another one',
-			'fourth one', 
+			'fourth one',
 			'fifth one'
 		];
 	}
